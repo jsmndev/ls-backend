eadfaab1bc40a5324a56265d549dde4e8f1fc61b
Jasmine Galang <jsmn.glng@gmail.com>
2023-08-26 21:16:28 -0500

    First stab of Calculator walkthrough

--- rb101/lesson2/calculator.rb
+++ rb101/lesson2/calculator.rb
@@ -1,36 +1,30 @@
-# Ask for two numbers
-puts ">> First number:"
-first_number = Kernel.gets().chomp().to_i()
+# ask the user for two numbers
+# ask the user for an operation to perform
+# perform the operation on the two numbers
+# output the result
 
-puts ">> Second number:"
-second_number = Kernel.gets().chomp().to_i()
+# answer = Kernel.gets()
+# Kernel.puts(answer)
 
-# Ask for the type of operation to perform: add, subtract, multiply or divide
-puts ">> Choose an opration:"
-puts "add, subtract, multiply or divide"
-operation = Kernel.gets().chomp()
+Kernel.puts("Welcome to Calculator!")
 
-result =
-  case operation.downcase
-  when "add"
-    first_number + second_number
+Kernel.puts("What's the first number?")
+number1 = Kernel.gets().chomp()
 
-  when "subtract"
-    first_number - second_number
+Kernel.puts("What's the second number?")
+number2 = Kernel.gets().chomp()
 
-  when "multiply"
-    first_number * second_number
+Kernel.puts("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
+operator = Kernel.gets().chomp()
 
-  when "divide"
-    first_number / second_number
-
-  else
-    nil
-  end
-
-# Display the result
-if result.nil?
-  puts "Invalid operation!"
+if operator == '1'
+  result = number1.to_i() + number2.to_i()
+elsif operator == '2'
+  result = number1.to_i() - number2.to_i()
+elsif operator == '3'
+  result = number1.to_i() * number2.to_i()
 else
-  puts "The result is: #{result}"
+  result = number1.to_f() / number2.to_f()
 end
+
+Kernel.puts("The result is #{result}")
