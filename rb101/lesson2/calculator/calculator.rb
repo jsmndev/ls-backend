e28f11d84e925ca882de87a48ea56289fd5bc0b5
Jasmine Galang <jsmn.glng@gmail.com>
2023-09-04 18:58:39 -0500

    Calculator Bonus Features

--- rb101/lesson2/calculator/calculator.rb
+++ rb101/lesson2/calculator/calculator.rb
@@ -3,12 +3,24 @@
 # perform the operation on the two numbers
 # output the result
 
+require 'yaml'
+
+MESSAGES = YAML.load_file('calculator_messages.yml')
+
 def prompt(message)
   puts "=> #{message}"
 end
 
-def valid_number?(num)
-  num.to_i != 0
+def valid_number?(input)
+  integer?(input) || float?(input)
+end
+
+def integer?(input)
+  input.to_i.to_s == input
+end
+
+def float?(input)
+  input.to_f.to_s == input
 end
 
 def operation_to_message(op)
@@ -22,54 +34,72 @@ def operation_to_message(op)
   messages[op]
 end
 
-prompt "Welcome to Calculator! Enter your name:"
+def valid_language?(input)
+  input == '1' || input == '2'
+end
+
+lang = ''
+loop do
+  prompt "Choose a language: 1) English 2) Tagalog"
+  lang = gets.chomp
+
+  break if valid_language?(lang)
+end
+
+messages =
+  case lang
+  when '1' then MESSAGES['en']
+  when '2' then MESSAGES['tl']
+  end
+
+prompt messages['welcome']
 
 name = ''
 loop do
   name = gets.chomp
 
   if name.empty?
-    prompt "Make sure to use a valid name."
+    prompt messages['valid_name']
   else
     break
   end
 end
 
-prompt("Hi #{name}!")
+prompt("#{messages['say_hi']} #{name}!")
 
 loop do # main loop
   number1 = ''
   loop do
-    prompt "What's the first number?"
+    prompt messages['first_number']
     number1 = gets.chomp
 
     if valid_number?(number1)
       break
     else
-      prompt "Hmm... that doesn't look like a valid number"
+      prompt messages['valid_number']
     end
   end
 
   number2 = ''
   loop do
-    prompt "What's the second number?"
+    prompt messages['second_number']
     number2 = gets.chomp
 
     if valid_number?(number2)
       break
     else
-      prompt "Hmm... that doesn't look like a valid number"
+      prompt messages['valid_number']
     end
   end
 
   operator_prompt = <<-OPERATOR_PROMPT
-    What operation would you like to perform?
     1) add
     2) subtract
     3) multiply
     4) divide
   OPERATOR_PROMPT
 
+  prompt messages['choose_operator']
   prompt(operator_prompt)
 
   operator = ''
@@ -79,7 +109,7 @@ loop do # main loop
     if %w(1 2 3 4).include?(operator)
       break
     else
-      prompt "Must choose 1, 2, 3 or 4"
+      prompt messages['valid_operator']
     end
   end
 
@@ -96,11 +126,11 @@ loop do # main loop
              number1.to_f / number2.to_f
            end
 
-  prompt "The result is #{result}"
+  prompt "#{messages['result']} #{result}"
 
-  prompt "Do you want to perform another calculation? (Y to calculate again)"
+  prompt messages['calculate_again']
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
 end
 
-prompt "Thank you for using the calculator. Good bye!"
+prompt messages['goodbye']
---
+++ rb101/small_problems/00_documentation_again/exercise2.md
@@ -0,0 +1,21 @@
+# Optional Arguments Redux
+Assume you have the following code:
+
+```ruby
+require 'date'
+
+puts Date.civil
+puts Date.civil(2016)
+puts Date.civil(2016, 5)
+puts Date.civil(2016, 5, 13)
+```
+
+What will each of the 4 `puts` statements print?
+
+Looking at the documentation, it said that it is the same as `Date.new`, and
+`Date.new` returns a new Date object.
+
+The 4 `puts` statements will print the following:
+
+```ruby
+```
