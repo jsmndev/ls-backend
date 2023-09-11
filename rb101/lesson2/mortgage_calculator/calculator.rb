bf757462c4d22155b3d521bccca5847be12c76e9
Jasmine Galang <jsmn.glng@gmail.com>
2023-09-11 16:52:50 -0500

    Finished Mortgage Calculator

--- rb101/lesson2/mortgage_calculator/calculator.rb
+++ rb101/lesson2/mortgage_calculator/calculator.rb
@@ -1,35 +1,29 @@
 # Mortgage Calculator
 
-# @TODO cover edge cases on these validation methods
-def valid_number?(input)
-  valid_integer?(input) || valid_float?(input)
+def prompt(message)
+  puts "=> #{message}"
 end
 
-def valid_integer?(input)
-  input.to_i.to_s == input
-end
-
-def valid_float?(input)
-  input.to_f.to_s == input
-end
-
+# this function builds out a user input and takes in
+# a hash with the following fields:
+#  - :prompt (String) - prompt the input's message
+#  - :err_msg (String) - prompt an error message
+#  - :validate_by (Symbol) - what kind of number input to validate.
+#       it accepts :by_integer or :by_float
 def get_input(attrs)
   loop do
-    puts ">> #{attrs[:prompt]}:"
+    prompt(attrs[:prompt])
     input = gets.chomp
 
     case attrs[:validate_by]
     when :by_integer
-      return input if valid_integer?(input)
+      return input if !input.empty? && input.to_i > 0
 
     when :by_float
-      return input if valid_float?(input)
-
-    when :by_number
-      return input if valid_number?(input)
+      return input if !input.empty? && input.to_f > 0
     end
 
-    puts ">> #{attrs[:err_msg]}"
+    prompt(attrs[:err_msg])
   end
 end
 
@@ -38,62 +32,99 @@ def get_interest_rate(apr)
   (apr / 100.0) / 12.0
 end
 
-# Get loan term (in months)
-def get_loan_term(years, months)
-  # Convert loan term in years to months and add it to
-  # the loan term in months
-  (years * 12) + months
+def calculate_monthly_payment(amount, interest_rate, term)
+  amount * (interest_rate / (1 - ((1 + interest_rate)**(-term))))
 end
 
-def calculate_monthly_payment(amount, apr, term_in_years, term_in_months)
-  # Get monthly interest rate
-  interest_rate = get_interest_rate(apr)
-  # Get loan term (in months)
-  loan_term = get_loan_term(term_in_years, term_in_months)
+def calculate_total_interest(amount, interest_rate, term, monthly_payment)
+  balance_remaining = amount
+  duration = term
+  total_interest = 0
 
-  # Calculate monthly payment
-  amount * (interest_rate / (1 - (1 + interest_rate)**(-loan_term)))
+  loop do
+    interest = balance_remaining * interest_rate
+    principle = monthly_payment - interest
+    balance_remaining -= principle
+    total_interest += interest
+    duration -= 1
+    break if duration == 0
+  end
+
+  total_interest
 end
+
+#
+# Program starts here
+#
+
+prompt("Welcome to Mortgage Calculator!")
+
 #
 # Get inputs
 #
 
-loan_amount = get_input({
-  prompt: "Enter loan amount",
-  err_msg: "Enter a valid amount",
-  validate_by: :by_integer
-}).to_i
+loop do
+  loan_amount = get_input(
+    {
+      prompt: "Enter loan amount:",
+      err_msg: "Enter a valid amount",
+      validate_by: :by_float
+    }
+  ).to_i
 
-apr = get_input({
-  prompt: "Enter APR (%)",
-  err_msg: "Enter a valid rate",
-  validate_by: :by_number
-}).to_f
+  apr = get_input(
+    {
+      prompt: "Enter APR (%):",
+      err_msg: "Enter a valid rate",
+      validate_by: :by_float
+    }
+  ).to_f
 
-puts ">> Enter loan term in years and/or in months:"
+  loan_term = get_input(
+    {
+      prompt: "Enter loan term (in months):",
+      err_msg: "Enter a valid term",
+      validate_by: :by_integer
+    }
+  ).to_i
 
-loan_term_in_years = get_input({
-  prompt: "in years",
-  err_msg: "Enter a valid term",
-  validate_by: :by_integer
-}).to_i
+  #
+  # Calculations
+  #
 
-loan_term_in_months = get_input({
-  prompt: "in months",
-  err_msg: "Enter a valid term",
-  validate_by: :by_integer
-}).to_i
+  # Get monthly interest rate
+  interest_rate = get_interest_rate(apr)
 
-#
-# Calculations
-#
+  # Calculate monthly payment
+  monthly_payment = calculate_monthly_payment(
+    loan_amount,
+    interest_rate,
+    loan_term
+  )
 
-# Calculate monthly payment
-monthly_payment = calculate_monthly_payment(
-                    loan_amount,
-                    apr,
-                    loan_term_in_years,
-                    loan_term_in_months
-                  )
+  # Calculate total payments
+  total_payments = monthly_payment * loan_term
 
-puts monthly_payment
+  # Calculate total interest
+  total_interest = calculate_total_interest(
+    loan_amount,
+    interest_rate,
+    loan_term,
+    monthly_payment
+  )
+
+  results = <<-RESULTS_PROMPT
+  Results:
+  Payment every month: $#{format('%.2f', monthly_payment)}
+  Total of #{loan_term} payments: $#{format('%.2f', total_payments)}
+  Total interest: $#{format('%.2f', total_interest)}
+  RESULTS_PROMPT
+
+  prompt(results)
+
+  prompt("Do you want to caculate again? (Y to calculate again)")
+  do_it_again = gets.chomp
+  break unless do_it_again.downcase.start_with?('y')
+end
+
+prompt("Thank you for using Mortgage Calculator, goodbye!")
